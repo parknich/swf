ee05286b77370904c5fa37b8321dc78081d25a1b
parknich <45834520+parknich@users.noreply.github.com>
2023-09-14 10:29:11 -0400

    sfdvdfv

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 document.getElementById("status").value = "js ran"
+console.log("js ran")
 const response = await fetch(
     "https://api.github.com/repos/parknich/swf/contents/swf/"
 );
@@ -8,23 +9,24 @@ const submitButton = document.getElementById("submit");
 const selectBox = document.getElementById("selectBox");
 const textarea = document.getElementById("textareaBox");
 statusBox.value = "running JS";
+console.log("running JS")
 const fileList = [];
-function populateSelect() {
-    selectBox.value = "select game";
-    for (let file of fileData) {
-        fileList.push(file.path);
-        option = new Option(file.name, file.path);
-        selectBox.appendChild(option)
-        statusBox.value = "adding; ${file.name} to dropdown box.";
-    }
+
+for (let file of fileData) {
+    fileList.push(file.path);
+    option = new Option(file.name, file.path);
+    selectBox.appendChild(option);
+    statusBox.value = "adding; ${file.name} to dropdown box.";
+    console.log("adding; ${file.name} to dropdown box.");
 };
-populateSelect();
 let swf;
 selectBox.onchange = () => {
     textarea.value = fileList[selectBox.value];
     swf = fileList[selectBox.value];
+    console.log("selectBox.onchange triggered");
 };
 
 submitButton.onclick = () => {
     statusBox.value = "button clicked"
+    console.log("button clicked")
 };
